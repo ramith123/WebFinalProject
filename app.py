569612ebf3f2f5b4d4836c18521a71b29e37a388
ChrisNJ <39505428+ChrisNJ@users.noreply.github.com>
2020-04-29 13:38:38 -0400

    updated register

--- app.py
+++ app.py
@@ -80,4 +80,4 @@ def loginTest():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, debug=True)
--- forms.py
+++ forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, RadioField, validators
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired, EqualTo, Email, Length
 
 class Register(FlaskForm):
@@ -7,7 +7,7 @@ class Register(FlaskForm):
     #email = StringField('email', validators=[Email(), InputRequired()])
     password = PasswordField('Password', validators=[Length(min=2, max=50), InputRequired(), EqualTo('confirm', message="Passwords must match.")])
     confirm = PasswordField('Repeat password', validators=[InputRequired()])
-    accept_tos = BooleanField('I accept the <a href="/tos">Terms of Service</a> and the <a href="/privacy"> Privacy Policy</a>', validators=[InputRequired()])
+    accept_tos = BooleanField('I accept the <a href="#tos">Terms of Service</a> and the <a href="#priv"> Privacy Policy</a>', validators=[InputRequired()])
     submit = SubmitField('Register', render_kw={'class': 'btn waves-effect waves-light white-text'})
 
 class Login(FlaskForm):
