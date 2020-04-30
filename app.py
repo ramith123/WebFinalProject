30af8be4fe6946b92c022d1b6226810fe9287db5
ChrisNJ <39505428+ChrisNJ@users.noreply.github.com>
2020-04-30 02:18:20 -0400

    Added a logout page etc

--- app.py
+++ app.py
@@ -102,7 +102,8 @@ def login():
 def logout():
     logout_user()
     flash("You have successfully logged out.")
-    return redirect(url_for("hello"))
+    #return redirect(url_for("hello"))
+    return render_template("logout.html")
 
 
 @app.route("/loginTest", methods=["GET"])
@@ -114,7 +115,7 @@ def loginTest():
 if __name__ == "__main__":
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)  # Remember to remove Debug
+    app.run(host="0.0.0.0", port=port, debug=True)  # Remember to remove Debug
 # =======
 # if __name__ == "__main__":
 #     # Bind to PORT if defined, otherwise default to 5000.
--- forms.py
+++ forms.py
@@ -10,25 +10,25 @@ class Register(FlaskForm):
         "Password",
         validators=[
             Length(
-                min=8, max=50, message="Password must be between 2 and 50 characters.",
+                min=8, max=50, message="Password must be between 8 and 50 characters.",
             ),
             InputRequired(),
             EqualTo("confirm", message="Passwords must match."),
             Regexp(
                 "^(?=.{8,})(?=.*[a-z])(?=.*[A-Z]).*$",
-                message="Must contain at least 1 UpperCase, 1 lowerCase.",
-            ),
-        ],
+                message="Must contain at least 1 UpperCase, 1 lowerCase."
+            )
+        ]
     )
     confirm = PasswordField("Repeat password", validators=[InputRequired()])
     accept_tos = BooleanField(
         'I accept the <a href="#tos">Terms of Service</a> and the <a href="#priv"> Privacy Policy</a>',
         validators=[InputRequired()],
-        render_kw={"class": "filled-in"},
+        render_kw={"class": "filled-in"}
     )
     submit = SubmitField(
         "Register",
-        render_kw={"class": "btn waves-effect waves-light amber darken-4 white-text"},
+        render_kw={"class": "btn waves-effect waves-light amber darken-4 white-text"}
     )
 
 
@@ -37,5 +37,5 @@ class Login(FlaskForm):
     password = PasswordField("Password", validators=[InputRequired()])
     submit = SubmitField(
         "Next",
-        render_kw={"class": "btn waves-effect waves-light white-text amber darken-4"},
+        render_kw={"class": "btn waves-effect waves-light white-text amber darken-4"}
     )
