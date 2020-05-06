e82801aa0eb8f04a59583b22ce9c9e0e2886cffb
ramith123 <ramithchandula@gmail.com>
2020-05-06 17:51:17 -0400

    commenting some code

--- initDB.py
+++ initDB.py
@@ -5,16 +5,16 @@ from model import db
 db.create_all(app=app)
 
 # temp test
-import deezerThings
-from model import User, Playlist
+# import deezerThings
+# from model import User, Playlist
 
 
-li = deezerThings.getSongsList("let it go")
-song = deezerThings.getSongModel(li[0])
-# user = User(username="hi", password="hi")
-# playlist = Playlist(name="playlist1", userid="1")
-user = User.query.filter_by(id="1").first()
-playlist = Playlist.query.filter_by(id="3").first()
+# li = deezerThings.getSongsList("let it go")
+# song = deezerThings.getSongModel(li[0])
+# # user = User(username="hi", password="hi")
+# # playlist = Playlist(name="playlist1", userid="1")
+# user = User.query.filter_by(id="1").first()
+# playlist = Playlist.query.filter_by(id="3").first()
 
 # # playlist.songs.append(song)
 # print(user.playlists)
