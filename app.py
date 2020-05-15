652d7f43e410793e9e4343735278c53b11b24582
ramith123 <ramithchandula@gmail.com>
2020-05-15 19:52:36 -0400

    logout button and some comments removed

--- app.py
+++ app.py
@@ -47,20 +47,6 @@ def create_app():
     app.config["SECRET_KEY"] = "c3a93f55-2015-4042-9ef7-77de85976f78"
     login_manager.init_app(app)
 
-    # app.config["YOUTUBE_API_KEY"] = "AIzaSyC0VqCv-KW7cRsmYBUUHHqTJeRBTVnP-h0" #Chris Good
-    # app.config["YOUTUBE_API_KEY"] = "AIzaSyDIk63q5hnaaQTLlPqLRPSrUYIYmLgMMTA" #Chris 2
-
-    # =======
-    # """ Begin boilerplate code """
-
-    # persistenceUrl = "postgres://hxzhttja:6A7fF17bjLUaeditu817xyU7x0AOzZTh@drona.db.elephantsql.com:5432/hxzhttja"
-    # def create_app():
-    #     app = Flask(__name__, static_url_path="")
-    #     app.config["SQLALCHEMY_DATABASE_URI"] = persistenceUrl
-    #     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
-    #     # app.config['SECRET_KEY'] = "c3a93f55-2015-4042-9ef7-77de85976f78"
-    #     # app.config['JWT_EXPIRATION_DELTA'] = timedelta(days = 7)
-    # >>>>>>> Ramith-working
     db.init_app(app)
     return app
 
@@ -74,12 +60,6 @@ app.app_context().push()
 def hello():
     playlist_url = "https://www.googleapis.com/youtube/v3/playlistItems"
     videos = []
-    # playlist_params = {
-    #     "key": app.config["YOUTUBE_API_KEY"],
-    #     "playlistId": "PL4fGSI1pDJn69On1f-8NAvX_CYlx7QyZc",  # Top 100 Music Videos United States(Playlist) on YouTube Music Global Charts channel",
-    #     "part": "snippet,contentDetails",
-    #     "maxResults": 50,
-    # }
 
     r = getPlaylistRequest()
     try:
@@ -230,43 +210,6 @@ def anyPageSearch():
     if request.method == "POST":
         query = request.form.get("queryBox")
         songList = getSongsList(query)
-
-        # search_params = {
-        #     "key": app.config["YOUTUBE_API_KEY"],
-        #     "q": query,
-        #     "part": "snippet",
-        #     "maxResults": 16,
-        #     "type": "video",
-        # }
-        # r = requests.get(search_url, params=search_params)
-        # results = r.json()["items"]
-
-        # video_ids = []
-        # for result in results:
-        #     video_ids.append(result["id"]["videoId"])
-
-        # video_params = {
-        #     "key": app.config["YOUTUBE_API_KEY"],
-        #     "id": ",".join(video_ids),
-        #     "part": "snippet,contentDetails",
-        #     "maxResults": 16,
-        # }
-
-        # r = requests.get(video_url, params=video_params)
-        # results = r.json()["items"]
-
-        # for result in results:
-        #     video_data = {
-        #         "id": result["id"],
-        #         "url": f"https://www.youtube.com/watch?v={ result['id'] }",
-        #         "thumbnail": result["snippet"]["thumbnails"]["high"]["url"],
-        #         "duration": int(
-        #             parse_duration(result["contentDetails"]["duration"]).total_seconds()
-        #             // 60
-        #         ),
-        #         "title": result["snippet"]["title"],
-        #     }
-        #     videos.append(video_data)
 
     return render_template("search.html", songs=songList)
 
